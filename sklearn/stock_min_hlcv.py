893ac6f3ae0a1a2487a412660d04612bb16c19a3
unknown <ngamc@yahoo.com>
2018-05-18 17:35:00 +0800

    modify stock_min_hlcv

--- sklearn/stock_min_hlcv.py
+++ sklearn/stock_min_hlcv.py
@@ -16,10 +16,8 @@ from pandas import read_csv
 df = read_csv('2018-5-17-one_min.txt', header=None,  delimiter=';', engine='python')
 df.columns=['datetime','open','high','low','close','volume']
 
-num=df.loc[df['datetime']=='2018/05/17/09/15/00'].index.values
+num=df.loc[df['datetime'].str.contains('09/15/00')].index.values
 
-print(type(num))
-print(num[0])
-df=df[num[0]:]
+df=df[num[0]+1:]
 print(df.iloc[0:5])
 
