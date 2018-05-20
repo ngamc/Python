68b973e931276a0a44bf17cba958cedb842c8326
unknown <ngamc@yahoo.com>
2018-05-20 22:19:17 +0800

    modify stock_min_hlcv `

--- sklearn/stock_min_hlcv.py
+++ sklearn/stock_min_hlcv.py
@@ -16,8 +16,11 @@ from pandas import read_csv
 df = read_csv('2018-5-17-one_min.txt', header=None,  delimiter=';', engine='python')
 df.columns=['datetime','open','high','low','close','volume']
 
-num=df.loc[df['datetime'].str.contains('09/15/00')].index.values
+# remove all entries before 09:16
+num=df.loc[df['datetime'].str.contains('09/16/00')].index.values
 
-df=df[num[0]+1:]
+df=df[num[0]:]
 print(df.iloc[0:5])
 
+x=df.values[:,1:6]
+print(x)
