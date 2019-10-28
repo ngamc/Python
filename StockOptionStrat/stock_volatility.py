9035ac18efe88d641876a7db112bb0ea6a76ed56
unknown <ngamc@yahoo.com>
2019-10-28 15:58:31 +0800

    Add stock_volatility

---
+++ StockOptionStrat/stock_volatility.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 28 15:37:03 2019
+This is to print the volatility of selected stocks
+
+@author: Nelson Cheung
+"""
+
+
+import time
+from datetime import datetime
+import pandas as pd
+import sys
+sys.path.append('D:\\user\\Documents\\Python\\')
